fa284749a619edaac82523c9d5af423e780f964a
Chris Colvard <cjcolvar@indiana.edu>
2015-07-16 11:22:12 -0400

    WIP Call FFMPEG working from some code from hydra-derivatives

--- lib/active_encode/engine_adapters/inline_adapter.rb
+++ lib/active_encode/engine_adapters/inline_adapter.rb
@@ -1,19 +1,33 @@
+require 'open3'
+
 module ActiveEncode
   module EngineAdapters
     class InlineAdapter
+      include Open3
+
       class_attribute :encodes, instance_accessor: false, instance_predicate: false
+      class_attribute :active_encodes, instance_accessor: false, instance_predicate: false
       InlineAdapter.encodes ||= {}
+      InlineAdapter.active_encodes ||= {}
+
+      FFMPEG_PATH = "/usr/local/ffmpeg-20130201/bin/ffmpeg"
 
       def create(encode)
         encode.id = SecureRandom.uuid
         self.class.encodes[encode.id] = encode
         #start encode
+        extension = encode.options.delete(:extension)
+        transcode(encode.input, encode.options, encode.options.delete(:output_file) || output_file(encode, extension))
+        active_encode[encode.id] = process_info
+        #TODO error handling!
         encode.state = :running
         encode
       end
 
       def find(id, opts = {})
         self.class.encodes[id]
+        #TODO fill in information from the ffmpeg command
+        #TODO CLOSE stdout/stderr when processing finished!
       end
 
       def list(*filters)
@@ -25,6 +39,7 @@ module ActiveEncode
         return if inline_encode.nil?
         inline_encode.state = :cancelled
         #cancel encode
+        self.class.active_encodes[encode.id][:thread].kill
         inline_encode
       end
 
@@ -36,6 +51,25 @@ module ActiveEncode
         inline_encode = self.class.encodes[encode.id]
         return if inline_encode.nil?
         inline_encode.output.delete(inline_encode.output.find {|o| o[:id] == output_id})
+      end
+
+      private
+      def output_file(encode, extension="mp4")
+        File.basename(encode.input).sub(File.extname(encode.input), '') + "." + extension
+      end
+
+      def transcode(path, options, out)
+        inopts = options.fetch(:input_options,[]).collect{|k,v| "-#{k} #{v}"}.join(" ")
+        inopts ||= "-y"
+        outopts = options.fetch(:output_options,[]).collect{|k,v| "-#{k} #{v}"}.join(" ")
+        outopts ||= options
+        execute "#{FFMPEG_PATH} #{inopts} -i \"#{path}\" #{outopts} #{out}"
+      end
+
+      def execute(command)
+        stdin, stdout, stderr, wait_thr = popen3(command)
+        stdin.close
+        {out: stdout, err: stderr, thread: wait_thr}
       end
     end
   end
